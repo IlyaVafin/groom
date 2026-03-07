06ed7c4025aa3a8994e2ab3195c68f39e55f1178
IlyaVafin <rthrthrer5gh@gmail.com>
2026-03-07 17:42:58 +0300

    feat: #2 add container, font Inter

--- frontend/src/modules/header/header.tsx
+++ frontend/src/modules/header/header.tsx
@@ -1,22 +1,37 @@
+import Container from "../../shared/ui/container/container"
 import styles from "./header.module.css"
 export default function Header() {
 	return (
 		<header>
-			<div className={styles.headerContainer}>
-				<nav className={styles.headerNav}>
-					<ul className={styles.headerList}>
-						<li className={styles.headerItem}>
-							<a href=''>Личный кабинет</a>
-						</li>
-						<li className={styles.headerItem}>
-							<a href=''>Зарегистрироваться</a>
-						</li>
-						<li className={styles.headerItem}>
-							<a href=''>Войти</a>
-						</li>
-					</ul>
-				</nav>
-			</div>
+			<Container>
+				<div className={styles.headerContainer}>
+					<div className={styles.logo}>
+						<img width='170' height='170' src='/svg/logo.svg' alt='логотип' />
+					</div>
+					<div className={styles.headerInfo}>
+						<nav className={styles.headerNav}>
+							<ul className={styles.headerList}>
+								<li className={styles.headerItem}>
+									<a href=''>Личный кабинет</a>
+								</li>
+								<li className={styles.headerItem}>
+									<a href=''>Зарегистрироваться</a>
+								</li>
+								<li className={styles.headerItem}>
+									<a href=''>Войти</a>
+								</li>
+							</ul>
+						</nav>
+						<div className={styles.headerContacts}>
+							<div className={styles.line}></div>
+							<div className=''>
+								<p>+7 904 760 88 90</p>
+								<p>Казань, ул. Пушкина 43</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</Container>
 		</header>
 	)
 }
---
+++ frontend/src/shared/ui/container/container.tsx
@@ -0,0 +1,5 @@
+import type { ReactNode } from "react"
+import styles from "./container.module.css"
+export default function Container({ children }: { children: ReactNode }) {
+	return <div className={styles.container}>{children}</div>
+}
