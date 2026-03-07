76078beaabcc2fb74f1f6c20148d77e8f1f03cde
IlyaVafin <rthrthrer5gh@gmail.com>
2026-03-07 18:17:48 +0300

    feat: #3 add routing, create desktop header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import Header from "./modules/header/header"
+import { Route, Routes } from "react-router"
+import Home from "./pages/home"
+import LoginPage from "./pages/login"
+import PersonalAccountPage from "./pages/personal-account"
+import RegisterPage from "./pages/register"
 
 const App = () => {
-  return (
-    <div>
-      <Header/>
-    </div>
-  )
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/personal-account' element={<PersonalAccountPage />} />
+			<Route path='/login' element={<LoginPage />} />
+			<Route path='/register' element={<RegisterPage />} />
+		</Routes>
+	)
 }
 
 export default App
--- frontend/src/modules/layout/header/header.tsx
+++ frontend/src/modules/layout/header/header.tsx
@@ -1,4 +1,5 @@
-import Container from "../../shared/ui/container/container"
+import { Link } from "react-router"
+import Container from "../../../shared/ui/container/container"
 import styles from "./header.module.css"
 export default function Header() {
 	return (
@@ -6,26 +7,28 @@ export default function Header() {
 			<Container>
 				<div className={styles.headerContainer}>
 					<div className={styles.logo}>
-						<img width='170' height='170' src='/svg/logo.svg' alt='логотип' />
+						<Link to='/'>
+							<img width='170' height='170' src='/svg/logo.svg' alt='логотип' />
+						</Link>
 					</div>
 					<div className={styles.headerInfo}>
 						<nav className={styles.headerNav}>
 							<ul className={styles.headerList}>
 								<li className={styles.headerItem}>
-									<a href=''>Личный кабинет</a>
+									<Link to='/personal-account'>Личный кабинет</Link>
 								</li>
 								<li className={styles.headerItem}>
-									<a href=''>Зарегистрироваться</a>
+									<Link to='/login'>Войти</Link>
 								</li>
-								<li className={styles.headerItem}>
-									<a href=''>Войти</a>
+								<li className={`${styles.headerItem} ${styles.signUp}`}>
+									<Link to='/register'>Зарегистрироваться</Link>
 								</li>
 							</ul>
 						</nav>
 						<div className={styles.headerContacts}>
 							<div className={styles.line}></div>
 							<div className=''>
-								<p>+7 904 760 88 90</p>
+								<p className={styles.phone}>+7 904 760 88 90</p>
 								<p>Казань, ул. Пушкина 43</p>
 							</div>
 						</div>
