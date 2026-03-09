fb5a192439c99ff1af7b053de98a9d4f5a6a6bef
IlyaVafin <rthrthrer5gh@gmail.com>
2026-03-09 14:03:40 +0300

    fix: #30 fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,10 +17,10 @@ app = FastAPI(lifespan=lifespan)
 
 origins = ["http://localhost:5173"]
 
-app.add_middleware(CORSMiddleware(allow_origins=origins, 
-                                  allow_credentials=True, 
-                                  allow_headers=["*"], 
-                                  allow_methods=["*"]))
+app.add_middleware(CORSMiddleware, allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],)
 
 app.include_router(user_router, prefix="/api", tags=["User"])
 app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
