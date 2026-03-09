67e173bdcd61e184cdb76e824f8a709ec55b486d
IlyaVafin <rthrthrer5gh@gmail.com>
2026-03-09 14:01:37 +0300

    feat: #29 add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,31 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from contextlib import asynccontextmanager
 from core.db import init_models
 from routes.user import user_router
 from routes.auth import auth_router
 from routes.order import order_router
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
   await init_models()
   yield
 app = FastAPI(lifespan=lifespan)
 
+origins = ["http://localhost:5173"]
+
+app.add_middleware(CORSMiddleware(allow_origins=origins, 
+                                  allow_credentials=True, 
+                                  allow_headers=["*"], 
+                                  allow_methods=["*"]))
+
 app.include_router(user_router, prefix="/api", tags=["User"])
 app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
 app.include_router(order_router, prefix="/api", tags=["Order"])
+
 @app.get("/")
 def get_root():
   return FileResponse("./img/kot.jpg")
